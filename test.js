343123266bbd1ff57c6c6e7fea2894619fc7f781
MA <rmaomina@gmail.com>
2018-11-17 23:46:48 +0900

    first-commit

---
+++ test.js
@@ -0,0 +1,27 @@
+class Animal {
+    constructor(name) {
+        this.speed = 0;
+        this.name = name;
+    }
+
+    run(speed) {
+        this.speed += speed;
+        alert(`${this.name} runs with speed ${this.speed}`);
+    }
+
+    stop() {
+        this.speed = 0;
+        alert(`${this.name} stopped.`);
+    }
+}
+
+class Rabbit extends Animal {
+    hide() {
+        alert(`${this.name} hides!`);
+    }
+}
+
+let rabbit = new Rabbit ('White Rabbit');
+
+rabbit.run(10);  //White rabbit runs 
+rabbit.stop();
